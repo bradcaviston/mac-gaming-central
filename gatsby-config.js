173ffbcf2e98a5f046bb717e9821c7d9d341b636
Brad Caviston <bradcaviston@Brads-iMac-Pro.local>
2020-03-22 13:55:22 -0400

    fixed images

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,6 @@ module.exports = {
     },
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`
   ],
