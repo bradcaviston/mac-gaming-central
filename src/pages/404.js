28775246343f689ff5d4711a345bb410005eb4cb
Brad Caviston <bradcaviston@Brads-iMac-Pro.local>
2020-03-22 09:33:07 -0400

    styled 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Layout from '../layouts/layout';
 import SEO from '../components/seo';
+import { Link } from 'gatsby';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{textAlign: 'center', paddingTop: '3rem'}}>
+      <h1 className="title is-1">404 NOT FOUND</h1>
+      <p style={{paddingBottom: '1.5rem'}}>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Link to="/" className="button is-primary">Go Home</Link>
+    </div>
   </Layout>
 );
 
